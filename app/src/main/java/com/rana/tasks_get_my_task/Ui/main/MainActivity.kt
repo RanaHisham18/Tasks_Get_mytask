a832cd0488ae972f1b7513f3f139deea747d5cb0
dell <hishamrana00@gmail.com>
2021-06-19 17:25:32 +0200

    modify xml

--- app/src/main/java/com/rana/tasks_get_my_task/Ui/main/MainActivity.kt
+++ app/src/main/java/com/rana/tasks_get_my_task/Ui/main/MainActivity.kt
@@ -6,10 +6,12 @@ import android.view.View
 import com.rana.tasks_get_my_task.R
 import com.rana.tasks_get_my_task.Ui.main.HomeState
 import com.rana.tasks_get_my_task.Ui.main.ViewModel
+import okhttp3.internal.concurrent.Task
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
